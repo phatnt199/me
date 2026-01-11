eb9736cb845f29b037866f28baa7cba1f1d978be
Phat Nguyen <tanphat199@gmail.com>
2026-01-11 12:21:15 +0700

    fix

--- react_project/vite.config.ts
+++ react_project/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Use '/' for custom domain, or '/repo-name/' for github.io/repo-name
+  base: '/me', // Use '/' for custom domain, or '/repo-name/' for github.io/repo-name
   plugins: [
     react(),
     VitePWA({
